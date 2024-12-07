b1394bc727023985a4ae0571d650ef362d72ec4e
Yishai Biton <yishai946@gmail.com>
2024-12-07 15:02:33 +0200

    works without validation

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import GuestForm from './GuestForm';
+
+function App() {
+  return (
+    <GuestForm />
+  );
+}
+
+export default App;
---
+++ src/GuestForm.jsx
@@ -0,0 +1,232 @@
+import React, { useState } from "react";
+import { db } from "./firebaseConfig";
+import {
+  doc,
+  setDoc,
+  getDocs,
+  collection,
+  query,
+  where,
+} from "firebase/firestore";
+
+const GuestForm = () => {
+  const [name, setName] = useState("");
+  const [isAttending, setIsAttending] = useState("");
+  const [guests, setGuests] = useState(1);
+  const [phone, setPhone] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+
+      const existingGuest = await getGuestByPhone(phone);
+      if (existingGuest.length > 0) {
+        alert("מספר הטלפון קיים כבר, נסה שוב");
+        return;
+      }
+
+      const timestamp = new Date().getTime().toString();
+      await setDoc(doc(db, "guests", timestamp), {
+        name,
+        isAttending,
+        guests: isAttending === "yes" ? parseInt(guests) : 0,
+        phone,
+      });
+
+      setFormSubmitted(true);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getGuestByPhone = async (phone) => {
+    const guestsRef = collection(db, "guests");
+    const q = query(guestsRef, where("phone", "==", phone));
+    const querySnapshot = await getDocs(q);
+
+    return querySnapshot.docs.map((doc) => doc.data());
+  };
+
+  const resetForm = () => {
+    setName("");
+    setIsAttending("");
+    setGuests(1);
+    setPhone("");
+    setFormSubmitted(false);
+  };
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.background}></div>
+      <div style={styles.content}>
+        {loading ? (
+          <div>Loading...</div>
+        ) : formSubmitted ? (
+          <div style={styles.successMessage}>
+            <h2>תודה רבה! פרטיך נשלחו בהצלחה.</h2>
+            <button onClick={resetForm} style={styles.button}>
+              שלח/י טופס נוסף
+            </button>
+          </div>
+        ) : (
+          <>
+            <h1 style={styles.title}>אליה & הללי</h1>
+            <p style={styles.description}>
+              אנו נרגשים להזמינך להצטרף לחגוג את אהבתנו ביום המיוחד שלנו. אנא
+              מלא/י את הפרטים כדי שנדע אם נזכה לראותך!
+            </p>
+            <form onSubmit={handleSubmit} style={styles.form}>
+              <label style={styles.label}>
+                שם מלא:
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  style={styles.input}
+                  placeholder="הקלד/י את שמך"
+                  required
+                />
+              </label>
+              <label style={styles.label}>
+                מספר טלפון:
+                <input
+                  type="phone"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  style={styles.input}
+                  placeholder="הקלד/י את מספר הטלפון שלך"
+                  required
+                />
+              </label>
+              <label style={styles.label}>
+                האם תגיע/י?
+                <select
+                  value={isAttending}
+                  onChange={(e) => setIsAttending(e.target.value)}
+                  style={styles.select}
+                  required
+                >
+                  <option value="" disabled>
+                    בחר/י
+                  </option>
+                  <option value="yes">כן</option>
+                  <option value="no">לא</option>
+                </select>
+              </label>
+              {isAttending === "yes" && (
+                <label style={styles.label}>
+                  כמה אנשים מגיעים (כולל אותך)?
+                  <input
+                    type="number"
+                    value={guests}
+                    onChange={(e) => setGuests(e.target.value)}
+                    min="1"
+                    style={styles.input}
+                    placeholder="מספר אורחים"
+                    required
+                  />
+                </label>
+              )}
+              <button type="submit" style={styles.button}>
+                שלח/י
+              </button>
+            </form>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    position: "relative",
+    minHeight: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f6d8d2",
+  },
+  background: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    background: "linear-gradient(to bottom right, #ff9a9e, #fad0c4, #fad0c4)",
+    zIndex: -1,
+    opacity: 0.9,
+  },
+  content: {
+    maxWidth: "600px",
+    padding: "30px",
+    borderRadius: "20px",
+    boxShadow: "0 5px 15px rgba(0, 0, 0, 0.2)",
+    backgroundColor: "#fffafc",
+    textAlign: "center",
+    fontFamily: "'Amatic SC', cursive",
+  },
+  title: {
+    fontSize: "36px",
+    color: "#c70039",
+    margin: "0",
+  },
+  description: {
+    fontSize: "18px",
+    color: "#444",
+    marginBottom: "20px",
+    lineHeight: "1.5",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  label: {
+    marginBottom: "10px",
+    fontSize: "20px",
+    color: "#333",
+  },
+  input: {
+    width: "90%",
+    padding: "10px",
+    fontSize: "18px",
+    marginTop: "5px",
+    marginBottom: "10px",
+    borderRadius: "10px",
+    border: "1px solid #ddd",
+  },
+  select: {
+    width: "90%",
+    padding: "10px",
+    fontSize: "18px",
+    marginTop: "5px",
+    marginBottom: "10px",
+    borderRadius: "10px",
+    border: "1px solid #ddd",
+    backgroundColor: "#f3f3f3",
+  },
+  button: {
+    padding: "10px 20px",
+    fontSize: "18px",
+    backgroundColor: "#c70039",
+    color: "#fff",
+    border: "none",
+    borderRadius: "20px",
+    cursor: "pointer",
+    transition: "background-color 0.3s",
+  },
+  successMessage: {
+    textAlign: "center",
+    color: "#333",
+    fontSize: "20px",
+  },
+};
+
+export default GuestForm;
